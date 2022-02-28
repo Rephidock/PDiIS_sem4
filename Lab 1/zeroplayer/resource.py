16b31f233975e245e2ddb1377779a3263cb3eaab
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-02-28 08:24:23 +0300

    Create resource.py

---
+++ Lab 1/zeroplayer/resource.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from zeroplayer.entity_killable import EntityKillable
+
+
+class Resource(EntityKillable):
+
+    # Class
+    _threshold: float = 0
+
+    # Instance
+    _value: float
+
+    def __init__(self, initial_value: float):
+        super().__init__()
+        self._value = initial_value
+
+    def take(self, value: float) -> None:
+        self._value -= value
+
+    def end_step(self) -> None:
+        # Check resource value
+        if self._value <= self.__class__._threshold:
+            self.kill()
+
+        # Move, then kill
+        super().end_step()
