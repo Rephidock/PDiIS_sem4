bbe648a5caa5d83a7fc470ab55d21c4a4e432cf6
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-01 22:38:14 +0300

    EntityKillable: renamed corpse -> residue

--- Lab 1/zeroplayer/entity_killable.py
+++ Lab 1/zeroplayer/entity_killable.py
@@ -7,8 +7,8 @@ from zeroplayer.spawn_rule import SpawnRule
 class EntityKillable(Entity):
 
     # Class variables
-    _corpse_rules: tuple[SpawnRule] = ()
-    _transfer_children: bool = False        # Only the last corpse entity will be used
+    _residue_rules: tuple[SpawnRule] = ()
+    _transfer_children: bool = False        # Only the last residue entity will be used
 
     # Instance variables
     __killed: bool
@@ -28,14 +28,14 @@ class EntityKillable(Entity):
             return
 
         # Create corpses
-        last_corpse: Optional[Entity] = None
-        for rule in self.__class__._corpse_rules:
-            last_corpse = rule.spawn_as_child(self.parent)
+        last_residue: Optional[Entity] = None
+        for rule in self.__class__._residue_rules:
+            last_residue = rule.spawn_as_child(self.parent)
 
         # Transfer children
         if self._transfer_children:
-            if not (last_corpse is None):
-                self.transfer_children(last_corpse)
+            if not (last_residue is None):
+                self.transfer_children(last_residue)
 
         # Unlink self
         if not (self.parent is None):
