aca9110dfe91fc4119c1a23d3ca14263191b8b0c
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-01 08:15:29 +0300

    Activator: Updated annotations

--- Lab 1/utils/activator.py
+++ Lab 1/utils/activator.py
@@ -1,9 +1,8 @@
-from __future__ import annotations
 from typing import Type, Any
 
 
 class Activator:
 
     @staticmethod
-    def create_instance(inst_type: Type, *args: Any):
+    def create_instance(inst_type: Type, *args: Any) -> Any:
         return inst_type(*args)
