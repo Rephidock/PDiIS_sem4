3d67c305c4d8d806b94767602233cd58456fa369
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-04 08:38:13 +0300

    Removed method queue. Added ActionPriorityQueue.

---
+++ Lab 1/zeroplayer/action_queue.py
@@ -0,0 +1,39 @@
+from typing import Callable, Any
+from queue import PriorityQueue
+from enum import IntEnum
+
+
+class StepPriority(IntEnum):
+
+    # Lower value - higher priority
+
+    PRE = 0
+    LIFETIME = 1
+    SPAWN = 7
+
+    BEGIN = 10
+
+    NORMAL = 20
+
+    END = 30
+    MOVE = 32
+    KILL = 35
+
+
+class ActionPriorityQueue:
+    """A priority queue of Callable actions"""
+
+    __actions: PriorityQueue[tuple[StepPriority, Any, Callable[[Any], None]]]
+
+    def __init__(self):
+        self.__actions = PriorityQueue()
+
+    def enqueue(self, priority: StepPriority, performer: Any, action: Callable[[Any], None]) -> None:
+        """Enqueues an action with priority"""
+        self.__actions.put((priority, performer, action))
+
+    def perform(self) -> None:
+        """Performs all actions from the queue, clearing the queue"""
+        while not self.__actions.empty():
+            item = self.__actions.get()
+            item[2](item[1])
