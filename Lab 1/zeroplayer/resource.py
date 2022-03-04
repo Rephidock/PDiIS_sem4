2c5ae77a47b650a1bc15e738fe1de9645e369549
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-04 12:12:27 +0300

    Resource: made value and requests private

--- Lab 1/zeroplayer/resource.py
+++ Lab 1/zeroplayer/resource.py
@@ -13,13 +13,13 @@ class Resource(EntityKillable):
     _decay: float = 0
 
     # Instance
-    _value: float
-    _requests: list[ResourceRequest]
+    __value: float
+    __requests: list[ResourceRequest]
 
     def __init__(self, initial_value: float):
         super().__init__()
-        self._value = initial_value
-        self._requests = list()
+        self.__value = initial_value
+        self.__requests = list()
 
     def step(self, queue: ActionPriorityQueue) -> None:
         queue.enqueue(StepPriority.DISTRIBUTE, self, self.__handle_distribution)
@@ -28,28 +28,28 @@ class Resource(EntityKillable):
         super().step(queue)
 
     def sign(self, taker: Entity, value: float, receiver: Callable) -> None:
-        self._requests.append(ResourceRequest(taker, value, receiver))
+        self.__requests.append(ResourceRequest(taker, value, receiver))
 
     def __handle_distribution(self) -> None:
         # Find total of requests value
         total_val = 0.0
-        for request in self._requests:
+        for request in self.__requests:
             total_val += request.value
 
         # Get multiplier for distribution
-        mult = min(1.0, self._value/total_val)
+        mult = min(1.0, self.__value/total_val)
 
         # Distribute
-        for request in self._requests:
-            self._value -= mult * request.value
+        for request in self.__requests:
+            self.__value -= mult * request.value
             request.receiver(mult * request.value)
 
     def __handle_decay(self) -> None:
-        self._value -= self._decay
+        self.__value -= self._decay
 
     def __handle_exhaustion(self) -> None:
         # Check resource value
-        if self._value <= self.__class__._threshold:
+        if self.__value <= self.__class__._threshold:
             self.kill()
 
 
