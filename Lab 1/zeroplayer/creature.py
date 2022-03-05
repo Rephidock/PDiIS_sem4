bc9e296bdf2acd4f989418291d20c72f7e693804
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-05 06:34:05 +0300

    Creature: Added _sign_to_resource_distribution; made _eat protected

--- Lab 1/zeroplayer/creature.py
+++ Lab 1/zeroplayer/creature.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
-from typing import Optional
+from typing import Optional, Type, Callable
 from zeroplayer.entity_movable import EntityMovable
 from zeroplayer.entity_killable import EntityKillable
+from zeroplayer.resource import Resource
 from zeroplayer.action_queue import StepPriority, ActionPriorityQueue
 
 
@@ -26,8 +27,33 @@ class Creature(EntityKillable, EntityMovable):
         queue.enqueue(StepPriority.DECAY, self, self.__handle_death_starvation)
         super().step(queue)
 
-    def eat(self) -> None:
+    def _eat(self) -> None:
         pass
+
+    def _sign_to_resource_distribution(
+            self,
+            resource_type: Type[Resource],
+            desired_amount: float,
+            receiver: Optional[Callable[[Creature, float], None]] = None
+    ):
+        """
+        Signs the creature for resource distribution
+        from all entities of a set type.
+
+        If receiver is None it is replaced with resource_intake
+        """
+        # Default Receiver
+        if receiver is None:
+            receiver = self._resource_intake
+
+        # Sign for distribution
+        resources = self.parent.children_by_type(resource_type)
+        requested_amount = desired_amount/len(resources)
+        for resource in resources:
+            resource.sign(self, requested_amount, receiver)
+
+    def _resource_intake(self, value: float):
+        self._satiety += value
 
     def __handle_hunger(self) -> None:
         # Hunger
@@ -35,7 +61,7 @@ class Creature(EntityKillable, EntityMovable):
 
         # Eating
         if self._satiety <= self._sated_threshold:
-            self.eat()
+            self._eat()
 
     def __handle_death_age(self) -> None:
         if (self._max_lifetime is not None) and (self.lifetime >= self._max_lifetime):
@@ -44,4 +70,3 @@ class Creature(EntityKillable, EntityMovable):
     def __handle_death_starvation(self) -> None:
         if self._satiety <= 0:
             self.kill()
-
