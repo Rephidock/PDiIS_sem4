4378a7687cb15a6ba19b865787706895bb48c98e
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-05 06:45:17 +0300

    Creature: resource intake is now controlled via class vars

--- Lab 1/zeroplayer/creature.py
+++ Lab 1/zeroplayer/creature.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Optional, Type, Callable
+from typing import Optional, Type
 from zeroplayer.entity_movable import EntityMovable
 from zeroplayer.entity_killable import EntityKillable
 from zeroplayer.resource import Resource
@@ -8,11 +8,17 @@ from zeroplayer.action_queue import StepPriority, ActionPriorityQueue
 
 class Creature(EntityKillable, EntityMovable):
 
-    # Class variables
+    # Aging
     _max_lifetime: Optional[int] = None
+
+    # Hunger
     _starting_satiety: float = 0.8
     _hunger_rate: float = 0.1
     _sated_threshold: float = 0.4  # 0.0 - 1.0
+
+    # Eating
+    _desired_resource: Type[Resource] = Resource
+    _resource_intake_mult: float = 1.0
 
     # Instance variables
     _satiety: float  # 0.0-1.0
@@ -30,30 +36,19 @@ class Creature(EntityKillable, EntityMovable):
     def _eat(self) -> None:
         pass
 
-    def _sign_to_resource_distribution(
-            self,
-            resource_type: Type[Resource],
-            desired_amount: float,
-            receiver: Optional[Callable[[Creature, float], None]] = None
-    ):
+    def _find_food(self, desired_amount: float):
         """
-        Signs the creature for resource distribution
-        from all entities of a set type.
-
-        If receiver is None it is replaced with resource_intake
+        Signs the creature for desired resource distribution
+        from all neighbouring resource entities.
         """
-        # Default Receiver
-        if receiver is None:
-            receiver = self._resource_intake
-
         # Sign for distribution
-        resources = self.parent.children_by_type(resource_type)
+        resources = self.parent.children_by_type(self._desired_resource)
         requested_amount = desired_amount/len(resources)
         for resource in resources:
-            resource.sign(self, requested_amount, receiver)
+            resource.sign(self, requested_amount, self._resource_intake)
 
     def _resource_intake(self, value: float):
-        self._satiety += value
+        self._satiety += value * self._resource_intake_mult
 
     def __handle_hunger(self) -> None:
         # Hunger
