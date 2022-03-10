4b1c3e84aae67ec59f8b72dd4a8166743f039e84
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-10 15:20:27 +0300

    Location: Added #regions and neighbours

--- Lab 1/zeroplayer/location.py
+++ Lab 1/zeroplayer/location.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from utils.action_queue import ActionPriorityQueue
 
 from zeroplayer.entity import Entity
@@ -9,14 +10,43 @@ class Location(Entity):
     """
     Represents a location.
     Can create new entities every step.
+
+    Location may have "neighbours", which are stored
+    in [id -> Location] dict.
     """
 
-    _rules: tuple[SpawnRule] = ()
+    def __init__(self):
+        super().__init__()
+        self.__init_neighbours()
 
     def step(self, queue: ActionPriorityQueue) -> None:
         queue.enqueue(StepPriority.SPAWN, self.__handle_spawn)
         super().step(queue)
 
+    #region //// Spawning
+
+    _rules: tuple[SpawnRule] = ()
+
     def __handle_spawn(self) -> None:
         for rule in self._rules:
             rule.spawn_as_child(self)
+
+    #endregion
+
+    #region //// Neighbours
+
+    neighbours: dict[int, Location]
+
+    def __init_neighbours(self):
+        self.neighbours = dict()
+
+    def add_neighbour(self, *args: Location):
+        for location in args:
+            self.neighbours[location.id] = location
+            location.neighbours[self.id] = self
+
+    def remove_neighbour(self, location: Location):
+        self.neighbours.pop(location.id)
+        location.neighbours.pop(self.id)
+
+    #endregion
