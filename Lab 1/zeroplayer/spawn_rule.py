5ec22544659e35e710c49b06249166431302ea07
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-10 15:37:23 +0300

    SpawnRule: fixed period

--- Lab 1/zeroplayer/spawn_rule.py
+++ Lab 1/zeroplayer/spawn_rule.py
@@ -20,18 +20,18 @@ class SpawnRule:
     def __init__(self,
                  entity_type: Type[Entity],
                  entity_args: tuple[Any] = (),
-                 period: int = 0,
+                 period: int = 1,
                  chance_of_spawn: float = 1.0
                  ):
         self.__entity_type = entity_type
         self.__entity_args = entity_args
-        self.__period = period + 1
+        self.__period = period
         self.__chance = chance_of_spawn
 
         self.__call_counter = 0
 
         if self.__period < 1:
-            raise ArithmeticError("")
+            raise ArithmeticError("Spawn period must be at least 1 step (equivalent of every step)")
 
     def spawn(self) -> Optional[Entity]:
         self.__call_counter += 1
