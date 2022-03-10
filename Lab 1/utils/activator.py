1d26f1d33d9f23053e7f631bdabd8818fb947fcd
Rephi <83082012+Rephidock@users.noreply.github.com>
2022-03-10 15:24:27 +0300

    Simplified activator to be just module, instead of class

--- Lab 1/utils/activator.py
+++ Lab 1/utils/activator.py
@@ -1,8 +1,5 @@
 from typing import Type, Any
 
 
-class Activator:
-
-    @staticmethod
-    def create_instance(inst_type: Type, *args: Any) -> Any:
-        return inst_type(*args)
+def create_instance(inst_type: Type, *args: Any) -> Any:
+    return inst_type(*args)
